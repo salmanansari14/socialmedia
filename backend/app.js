9d149f0caf7e5599333f55f7d4032f9eab326229
slaman <slamanansari1409@gmail.com>
2024-02-09 00:55:51 +0530

    fixback

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,9 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
+app.post("/loginn", (req, res)=>{
+    res.send("hello salman")
+})
 
 const post = require("./routes/post");
 const user = require("./routes/user");
@@ -18,7 +21,6 @@ const user = require("./routes/user");
 
 app.use("/api/v1", post);
 app.use("/api/v1", user);
-app.get("jkghsghgh")
 
 app.use(express.static(path.join(__dirname, "../frotend/build")));
 app.get("*", (req, res) => {
