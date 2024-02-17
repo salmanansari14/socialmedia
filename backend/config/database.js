79fb6b1e7fa381f403aac92b2d5a48201e4dcc4c
slaman <slamanansari1409@gmail.com>
2024-02-18 00:32:01 +0530

    aaaa

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const MONGO_URI = 'mongodb+srv://salmanansari:Salman123@cluster0.t8a6cmf.mongodb.net/SocialApp'
+// const MONGO_URI = 'mongodb+srv://salmanansari:Salman123@cluster0.t8a6cmf.mongodb.net/SocialApp'
 exports.connectDatabase = () =>{
-    mongoose.connect(MONGO_URI)
+    mongoose.connect("mongodb+srv://salmanansari:Salman123@cluster0.t8a6cmf.mongodb.net/SocialApp")
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 }
