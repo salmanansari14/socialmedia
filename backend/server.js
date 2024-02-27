7710beaf3733283a6f11dd291ae797b114a4c169
slaman <slamanansari1409@gmail.com>
2024-02-27 14:18:56 +0530

    fixcors1

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,20 @@ const cors = require("cors");
 const { connectDatabase } = require("./config/database");
 const cloudinary = require("cloudinary")
 const PORT = process.env.PORT || 6010
-app.use(cors());
 
 connectDatabase();
 
 // cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET,
-// })
-cloudinary.config({
-    cloud_name: "dovc5xwtu",
-    api_key: "644328533923261",
-    api_secret: "_nFmzD4RIeUWzl-mjHilRbn7jzw",
-})
+    //     cloud_name: process.env.CLOUDINARY_NAME,
+    //     api_key: process.env.CLOUDINARY_API_KEY,
+    //     api_secret: process.env.CLOUDINARY_API_SECRET,
+    // })
+    cloudinary.config({
+        cloud_name: "dovc5xwtu",
+        api_key: "644328533923261",
+        api_secret: "_nFmzD4RIeUWzl-mjHilRbn7jzw",
+    })
+    app.use(cors());
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
