509470ab4f7d15ebc5401c6eced756dfe3c50e6a
slaman <slamanansari1409@gmail.com>
2024-02-29 00:01:20 +0530

    asdd

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,10 @@ const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config({ path: "backend/config/config.env" });
 }
