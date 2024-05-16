63863ab47907726cdd91389ae8dcabc3300fe66b
salmanansari.14 <salmanansari1409@gmail.com>
2024-05-17 02:45:24 +0530

    salman

--- backend/app.js
+++ backend/app.js
@@ -22,8 +22,8 @@ const user = require("./routes/user");
 app.use("/api/v1", post);
 app.use("/api/v1", user);
 
-// app.use(express.static(path.join(__dirname, "../frotend/build")));
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// });
+app.use(express.static(path.join(__dirname, "../frotend/build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 module.exports = app
