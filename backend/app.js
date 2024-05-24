0b4824ce82c9e7ba27ddee26c367d806ea1897e1
salmanansari.14 <salmanansari1409@gmail.com>
2024-05-25 02:25:26 +0530

    skdhd

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
-const path = require("path");
+// const path = require("path");
 app.use(cors({
     origin: [""],
     methods: ["POST", "GET"],
@@ -21,14 +21,14 @@ const user = require("./routes/user");
 
 app.use("/api/v1", post);
 app.use("/api/v1", user);
-app.get("/", (req, res)=>{
+app.get("*", (req, res)=>{
     res.send('lfghk')
 });
 
-app.use(express.static(path.join(__dirname, "../frotend/build")));
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "../frotend/build")));
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 module.exports = app
 
 
