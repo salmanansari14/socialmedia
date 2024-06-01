13c342b5e35bf7da90cd6bc1bb41d6257ae2ced0
salmanansari.14 <salmanansari1409@gmail.com>
2024-06-02 02:31:24 +0530

    fixCors

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,19 @@ const app = express();
 const cookieParser = require("cookie-parser");
 // const path = require("path");
 function setCorsHeaders(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  }
-app.use(cors( setCorsHeaders));
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+}
+app.use(cors());
+app.use(setCorsHeaders)
 
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config({ path: "backend/config/config.env" });
+  require("dotenv").config({ path: "backend/config/config.env" });
 }
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -23,18 +27,18 @@ const user = require("./routes/user");
 
 app.use("/api/v1", post);
 app.use("/api/v1", user);
-app.get("/", (req, res)=>{
-    res.send('lfghk')
+app.get("/", (req, res) => {
+  res.send("lfghk");
 });
 
 // app.use(express.static(path.join(__dirname, "../frotend/build")));
 // app.get("*", (req, res) => {
 //     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 // });
-module.exports = app
+module.exports = app;
 
 // "version": 2,
-//     "build": 
+//     "build":
 //         {
 //         },
 //     "routes": [
