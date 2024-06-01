1db6705f0de9317c492e14a6f469e2918b32f012
salmanansari.14 <salmanansari1409@gmail.com>
2024-06-02 02:42:20 +0530

    fixCors

--- backend/app.js
+++ backend/app.js
@@ -15,9 +15,9 @@ function setCorsHeaders(req, res, next) {
 app.use(cors());
 app.use(setCorsHeaders)
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: "backend/config/config.env" });
-}
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config({ path: "backend/config/config.env" });
+// }
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
